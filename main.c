9e29bf1b789f3d37565657b844b7e7276556f90e
Andrey Markov <markov@programmer.net>
2018-10-03 02:19:13 +0300

    Много изменений. + Теперь присутствуют фукнкции добавления, изменения, удаления, а также отображения сеансов. + Все функции рабочие, оптимизированные. - Всё ещё нет главного меню.

--- main.c
+++ main.c
@@ -4,8 +4,6 @@
 #include <string.h>
 #include <time.h>
 
-FILE *file;
-
 struct _session {
     struct tm dt;
     char cinema[CHAR_MAX];
@@ -15,10 +13,12 @@ struct _session {
 } session;
 
 int add() {
+    FILE *fp = fopen("db", "wb");
+
     // date
     char date[CHAR_MAX];
     do {
-        printf("Введите дату начала сеанса (ДД:ММ:ГГ): ");
+        printf("Введите дату начала сеанса (ДД.ММ.ГГ): ");
         scanf("%s", date);
     } while (!strptime(date, "%d.%m.%y", &session.dt));
 
@@ -50,27 +50,119 @@ int add() {
              strcmp(session.type, "Обычный"));
 
     // write to file
-    fwrite(&session, sizeof(struct _session), 1, file);
+    fwrite(&session, sizeof(struct _session), 1, fp);
+
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
+
+int edit(int n) {
+    FILE *fp = fopen("db", "r+b");
+
+    for (int i = 1; i <= n; i++) {
+        if (!fread(&session, sizeof(struct _session), 1, fp)) {
+            printf("Записи под таким номером не существует.\n");
+            fclose(fp);
+            return EXIT_SUCCESS;
+        }
+    }
+
+    // date
+    char date[CHAR_MAX];
+    strftime(date, CHAR_MAX, "%d.%m.%y", &session.dt);
+    do {
+        printf("Дата начала сеанса (%s): ", date);
+        scanf("%s", date);
+    } while (!strptime(date, "%d.%m.%y", &session.dt));
+
+    // time
+    char time[CHAR_MAX];
+    strftime(time, CHAR_MAX, "%H:%M", &session.dt);
+    do {
+        printf("Время начала сеанса (%s): ", time);
+        scanf("%s", time);
+    } while (!strptime(time, "%H:%M", &session.dt));
+
+
+    // cinema
+    printf("Название кинотеатра (%s): ", session.cinema);
+    scanf("\n%[^\n]", &session.cinema);
+
+    // film
+    printf("Название фильма (%s): ", session.film);
+    scanf("\n%[^\n]", &session.film);
+
+    // hall
+    printf("Номер зала (%i): ", session.hall);
+    scanf("%i", &session.hall);
+
+    // type
+    do {
+        printf("Тип сеанса (%s): ", session.type);
+        scanf("%s", &session.type);
+    } while (strcmp(session.type, "3D") &
+             strcmp(session.type, "Imax") &
+             strcmp(session.type, "Обычный"));
+
+
+    // write to file
+    fseek(fp, -(sizeof(struct _session)), SEEK_CUR);
+    fwrite(&session, sizeof(struct _session), 1, fp);
+
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
+
+int rem(int n) {
+    FILE *fp = fopen("db", "rb");
+    FILE *fp_ = fopen("db_", "wb");
+    struct _session session_;
+
+    int i = 1;
+    while (fread(&session, sizeof(struct _session), 1, fp)) {
+        if (i != n) {
+            memcpy(&session_, &session, sizeof(struct _session));
+            fwrite(&session_, sizeof(struct _session), 1, fp_);
+        };
+        i++;
+//            printf("Записи под таким номером не существует.\n");
+//            return EXIT_SUCCESS;
+    }
+
+    fclose(fp);
+    fclose(fp_);
+
+    remove("db");
+    rename("db_", "db");
 
     return EXIT_SUCCESS;
 }
 
 int show() {
-    char dt[CHAR_MAX], i = 1;
-    while (fread(&session, sizeof(struct _session), 1, file)) {
-            strftime(dt, CHAR_MAX, "%d.%m.%y, %H:%M", &session.dt);
-            printf("%i: %s, %s, %s, зал №%i, %s\n",
-                   i++, dt, session.cinema, session.film,
-                   session.hall, session.type);
+    FILE *fp = fopen("db", "rb");
+
+    char date[CHAR_MAX], time[CHAR_MAX], i = 1;
+    while (fread(&session, sizeof(struct _session), 1, fp)) {
+        strftime(date, CHAR_MAX, "%d.%m.%y", &session.dt);
+        strftime(time, CHAR_MAX, "%H:%M", &session.dt);
+        printf("%i: %s, %s, %s, %s, зал №%i, %s\n",
+           i++, date, time, session.cinema, session.film,
+           session.hall, session.type);
     };
+
+    fclose(fp);
+
+    return EXIT_SUCCESS;
 }
 
 int main () {
-    file = fopen("db", "a+b");
+    //fp = fopen("db", "a+b");
 
     //add();
     show();
 
-    fclose(file);
+    //fclose(fp);
 	return EXIT_SUCCESS;
 }
